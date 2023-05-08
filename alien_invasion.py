c7b020a3a75da6b852c7357822b2b82403622271
Cj <Cj@gmail>
2023-05-08 21:33:01 +0800

    Aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -21,6 +22,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """开始游戏的主循环。"""
@@ -93,8 +97,16 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
+    
+    def _create_fleet(self):
+        """创建外星人群。"""
+        #创建一个外星人。
+        alien = Alien(self)
+        self.aliens.add(alien)
+
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏。
